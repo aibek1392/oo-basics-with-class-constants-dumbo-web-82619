5248d19f18feaccb4e6f3728dc97fe03530047cb
Aibek Ozhorov <ozhorov@gmail.com>
2019-09-02 22:19:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,12 @@
 class Shoe
   attr_accessor :color,  :size, :material, :condition
-  attr_reader :brand 
+  attr_reader :brands 
    BRANDS = []
 
- def initialize(brand)
+ def initialize(brands)
 
- @brand = brand
- BRAND << brand
+ @brand = brands
+ BRAND << brands
  end
  
 
